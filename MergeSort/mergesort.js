64296911c4911495ffc10d441804b9701ad90d65
Jee Zhou <jeezhou@Jees-iMac.local>
2020-09-14 20:38:33 -0500

    work on algos

---
+++ MergeSort/mergesort.js
@@ -0,0 +1,31 @@
+/*
+What is a heap?
+
+
+*/
+
+
+
+
+
+
+let randomArray = [];
+//Write a function to generate a random array
+function genRandomArray(array){
+  for (var i = 0; i < 20; i++){
+    array.push(Math.floor(Math.random()*300));
+  }
+};
+
+//Run the mergesort algorithm:
+
+
+
+function mergeSort(array){
+  //mergesort here:
+
+}
+
+
+genRandomArray(randomArray);
+console.log(randomArray);
