41adad56e9445a643774fe01b501876d67abfc5e
Jee Zhou <jeezhou@Jees-iMac.local>
2020-09-15 20:21:36 -0500

    work on heapsort

--- HeapSort/heapsort.js
+++ HeapSort/heapsort.js
@@ -25,8 +25,15 @@ So two questions: 1. given an arbitrary array, how to turn it into a max heap?
                   2. How to extract max out of the max heap and still keep the max heap property?
 
 Operations to consider for time-complexity:
-1. Build max-heap given an arbitrary array.  To accomplish building a max-heap, we consider the operation max-heapify: correct a SINGLE violation of the max-heap property in a subtree's root.
 
-//Finish the rest here!
+1. Build max-heap given an arbitrary array.  To accomplish building a max-heap, we consider the operation max-heapify.
+   max-heapify is ran on an array A with an index i, and the precondition is that the left subtree rooted at index i (left(i)) and the right subtree rooted at index i (right(i)) are both max-heaps.  We typically denote this opeartion using an ordered pair: (A, i).
 
-                  */
+   What does max-heapify do? 
+   In other words, assume we are given an array that is not quite a max-heap, but almost one, meaning it only contains 1 single violation of the max-heap property and running max-heapify will fix such violation.
+
+   Looking ahead, if we do this recursively at different levels of the tree, then we impose the max-heap property to an array.
+
+So for example, if we run 
+
+*/
