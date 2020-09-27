aee2eb8c11ff56f3c64092cc9aaedd623af7ef4b
Jee Zhou <jwhiz113@gmail.com>
2020-09-27 09:40:36 -0500

    work on detect loops

---
+++ LinkedList/DetectIntersection.js
@@ -0,0 +1,145 @@
+//DEBUG THIS SOLUTION!
+class Node{
+  constructor(value, next=null){  //a node takes a value and a next pointer, it is by default null.
+    this.value = value;
+    this.next = next;
+  }
+}
+//declare a new object to store the  pointer and value of the last node:
+class Last {
+  constructor(pointer=null, size=0){
+    this.tail = pointer;
+    this.size = size;
+  }
+}
+class LinkedList {
+  constructor(){
+    this.head = null;  
+  }
+
+  add(value){
+    var node = new Node(value);
+    var current;
+    if (this.head == null){
+      this.head = node;
+    }else {
+      current = this.head; //get current to point to the start of the linked list
+      while(current.next){
+        current = current.next;
+      } //When the while loop finishes, current is pointing at the last node.
+      current.next = node;  //get the next pointer to point at the newly created node.
+    }
+  };
+  remove(value){  //assume head is not null
+    if (this.head.value == value){
+      this.head = this.head.next;
+    }
+    var current = this.head;
+    while(current.next != null){  //search for the next value and see if it matches
+      if (current.next.value == value){
+        current.next = current.next.next;
+        //console.log('found it!')
+      }
+      current = current.next;
+    }
+  }
+  printList(){
+    var current = this.head;
+    var str = "";
+    while(current){
+      str += current.value + " ";
+      current = current.next;
+    }
+    console.log(str);
+  }
+  //Given two LLs, determine if the two lists intersect.  Return the intersecting node.  For example:
+  /*
+    3->1->5->9->7->2->1
+    4->6->->7->2->1
+    The function should return the variable pointing at the node 7.  (note that insertion means pointer references.)
+
+    One approach would just be use a set and throw all the pointers in one LL in and walk through another LL and return the first one that matches, cause once one pointer matches, the rest of the LL would have to match.  This approach takes O(N) time.
+
+    Another approach would be to determine the lengths of the two LLs.  Now if this was given to us, then we simply need to chop off the initial part of the longer LL and walk along with the other LL and compare their pointers, if we have a match then we are done and return such pointer.
+
+    If the length was not given to us, then we have to do a bit more work and walk along the two LLs to find the lengths.
+  */
+  
+}
+
+function getTailandSize(ll){
+  var current = ll.head;
+  
+  if (current == null){
+    return new Last();
+  }
+  var size =1;
+  while (current.next != null){
+    current = current.next;
+    size++;
+  }
+  return new Last(current, size);
+};
+
+function findIntersect(ll1, ll2){
+  var last1 = getTailandSize(ll1);
+  var last2 = getTailandSize(ll2);
+  if (last1.tail != last2.tail){  //if the tails are pointing at different nodes, then no intersecting nodes.
+    return null;
+  }
+  //case when tails are pointer at the same nodes.
+  if (last1.size == last2.size){
+    return walkAndFindIntersect(last1.tail, last2.tail);
+  }else if (last1.size > last2.size){
+    var difference = last1.size - last2.size;
+    for (var i = 1; i <= difference; i++){
+      last1.tail = last1.tail.next;
+    }
+    return walkAndFindIntersect(last1.tail, last2.tail);
+  }else if (last2.size > last1.size){
+    var difference = last2.size - last1.size;
+    for (var i = 1; i <= difference; i++){
+      last2.tail = last2.tail.next;
+    }
+    return walkAndFindIntersect(last1.tail, last2.tail);
+  }
+  
+}
+
+function walkAndFindIntersect(node1, node2){
+  while (node1 != null && node2 != null){
+    if (node1 == node2){
+      return node1;
+    }else {
+      node1 = node1.next;
+      node2 = node2.next;
+    }
+  }
+}
+
+var ll1= new LinkedList();  
+var ll2 = new LinkedList();
+// adding element to the list 
+ll1.add(10);  
+// prints 10 
+
+// adding more elements to the list 
+ll1.add(20); 
+ll1.add(50); 
+ll1.add(30); 
+ll1.add(20); 
+ll1.add(10);  
+ll1.add(40); 
+ll1.add(30);
+ll1.add(50); 
+
+ll2.add(80);
+ll2.add(10);
+ll2.add(20);
+ll2.add(30);
+ll2.add(40);
+ll2.add(90);
+var tailPointer = getTailandSize(ll2);
+tailPointer.tail.next = ll1.head;
+var intersect = findIntersect(ll1, ll2);
+console.log(tail);
---
+++ LinkedList/Palindrome.js
@@ -0,0 +1,83 @@
+class Node{
+  constructor(value, next=null){  //a node takes a value and a next pointer, it is by default null.
+    this.value = value;
+    this.next = next;
+  }
+}
+class LinkedList {
+  constructor(){
+    this.head = null;  
+  }
+  add(value){
+    var node = new Node(value);
+    var current;
+    if (this.head == null){
+      this.head = node;
+    }else {
+      current = this.head; //get current to point to the start of the linked list
+      while(current.next){
+        current = current.next;
+      } //When the while loop finishes, current is pointing at the last node.
+      current.next = node;  //get the next pointer to point at the newly created node.
+    }
+  };
+  remove(value){  //assume head is not null
+    if (this.head.value == value){
+      this.head = this.head.next;
+    }
+    var current = this.head;
+    while(current.next != null){  //search for the next value and see if it matches
+      if (current.next.value == value){
+        current.next = current.next.next;
+        //console.log('found it!')
+      }
+      current = current.next;
+    }
+  }
+  printList(){
+    var current = this.head;
+    var str = "";
+    while(current){
+      str += current.value + " ";
+      current = current.next;
+    }
+    console.log(str);
+  }
+  reverseList(){
+    var ll = new LinkedList();
+    var current = this.head;
+    while(current != null){
+      var node = new Node(current.value);
+      node.next = ll.head;  //points the next element to the previously made node
+      ll.head =node;  //head is now the new node
+      current = current.next; //move current along
+    }
+    return ll;
+  }
+}
+//Problem: Implement a function to check if a LL is a palindrome.
+function isPalindrome(ll1){
+  //idea: reverse the LL in a buffer and then compare it with the original LL.
+  var reverse = ll1.reverseList();
+  var l1Current = ll1.head;
+  var l2Current = reverse.head;
+  while(l1Current != null && l2Current != null){
+    if (l1Current.value != l2Current.value){
+      return false;
+    }
+    l1Current = l1Current.next;
+    l2Current =l2Current.next;
+  }
+  return l1Current == null & l2Current == null;
+}
+var ll = new LinkedList();
+ll.add(10);  
+ll.add(20); 
+ll.add(50); 
+ll.add(30); 
+ll.add(30); 
+ll.add(50);  
+ll.add(20); 
+ll.add(10);
+console.log(isPalindrome(ll));  //true gives a value of 1
+//console.log();
